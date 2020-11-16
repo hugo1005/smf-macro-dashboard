b745c469f15b7cbdae1b6291dbfdc3801c746d46
Hugo Dolan <hugojdolan@gmail.com>
2020-11-16 18:38:38 +0000

    =fixed

--- flaskapp.py
+++ flaskapp.py
@@ -12,7 +12,7 @@ import scraper as sc
 DEBUG = True
 
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
 app.config.from_object(__name__)
 
 # enable CORS
