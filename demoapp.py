c7f10fb00464077818e62fa0fc4b626eff95e18f
Hugo Dolan <hugojdolan@gmail.com>
2020-12-30 14:00:04 +0000

    =

--- demoapp.py
+++ demoapp.py
@@ -18,26 +18,26 @@ if __name__ == "__main__":
 def hello():
     return app.send_static_file("index.html")
 
-@app.route('/chart_data', methods=['GET'])
-def chart_data():
-    print("Serving chart data...")
-    global last_updated_macro, macro_data_obj, one_day
-    print("Fetching chart data", time.time(), last_updated_macro, time.time() - last_updated_macro)
-    if time.time() - last_updated_macro > one_day: 
-        print("Updating Macro Data")
-        macro_data_obj = sc.scrape_data()
-        last_updated_macro = time.time() 
+# @app.route('/chart_data', methods=['GET'])
+# def chart_data():
+#     print("Serving chart data...")
+#     global last_updated_macro, macro_data_obj, one_day
+#     print("Fetching chart data", time.time(), last_updated_macro, time.time() - last_updated_macro)
+#     if time.time() - last_updated_macro > one_day: 
+#         print("Updating Macro Data")
+#         macro_data_obj = sc.scrape_data()
+#         last_updated_macro = time.time() 
 
-    return jsonify(macro_data_obj)
+#     return jsonify(macro_data_obj)
 
-@app.route('/risk_data', methods=['GET'])
-def risk_data():
-    print("Serving risk data...")
-    global last_updated_risk, risk_data_obj, one_day
+# @app.route('/risk_data', methods=['GET'])
+# def risk_data():
+#     print("Serving risk data...")
+#     global last_updated_risk, risk_data_obj, one_day
 
-    if time.time() - last_updated_risk > one_day: 
-        print("Updating Risk Data")
-        risk_data_obj = ri.get_attribution_report()
-        last_updated_risk = time.time() 
+#     if time.time() - last_updated_risk > one_day: 
+#         print("Updating Risk Data")
+#         risk_data_obj = ri.get_attribution_report()
+#         last_updated_risk = time.time() 
 
-    return jsonify(risk_data_obj)
+#     return jsonify(risk_data_obj)
