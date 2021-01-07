e3292af8cba496467e22b78e9c778a13f3c27cec
Hugo Dolan <hugojdolan@gmail.com>
2021-01-07 16:40:39 +0000

    =

--- risk.py
+++ risk.py
@@ -662,6 +662,38 @@ def get_attribution_report():
         
     print("Fetching attribution report completed!")
 
+
+    equity_sectors # Maps Sector -> Stock
+    equity_regions # Maps Region -> Stock
+    approximate_sector_weights # Maps Region -> Sector -> Benchmark Weight
+    portfolio_equity_weights # Maps Stock -> Weight
+
+    # Compute Tree 
+    # Sector -> Region (EU,US) Value Port and Value Benchmark
+    map_region_names = {
+        'SPY': 'US',
+        46323: 'EU'
+    }
+
+    weight_tree = {
+        "children": [
+            { 
+                "name": sector,
+                "children": [
+                    {
+                        "name": map_region_names[region],
+                        "port_weight": sum([portfolio_equity_weights[equity] for equity in equities]),
+                        "benchmark_weight": approximate_sector_weights[region][sector],
+                        "colname":"level3"
+                    } for region, equities in equity_regions.items()
+                ],
+                "colname":"level2"
+            
+            } for sector in equity_sectors
+        ],
+        "name": "PORTFOLIO"
+    }
+
     return {
         "portfolio_summary_stats": {
             "sharpe": sharpe,
@@ -669,6 +701,7 @@ def get_attribution_report():
             "beta": beta,
             "tracking_error_pct": tracking_error_pct,
         },
+        "weightings": weight_tree,
         "attribution": {
             "regions":{
                 "Overall": json.loads(attribution['overall']['aggregate_model_df'].to_json())['Returns (%)'],
@@ -682,3 +715,18 @@ def get_attribution_report():
         },
         "equities": export_equities
     }
+
+
+{"children":
+[
+    {
+        "name":"boss1",
+        "children":[
+        {"name":"mister_a","group":"A","value":28,"colname":"level3"},
+        {"name":"mister_b","group":"A","value":19,"colname":"level3"},
+        {"name":"mister_c","group":"C","value":18,"colname":"level3"},
+        {"name":"mister_d","group":"C","value":19,"colname":"level3"}],
+        "colname":"level2"
+    },
+    {"name":"boss2","children":[{"name":"mister_e","group":"C","value":14,"colname":"level3"},{"name":"mister_f","group":"A","value":11,"colname":"level3"},{"name":"mister_g","group":"B","value":15,"colname":"level3"},{"name":"mister_h","group":"B","value":16,"colname":"level3"}],"colname":"level2"},{"name":"boss3","children":[{"name":"mister_i","group":"B","value":10,"colname":"level3"},{"name":"mister_j","group":"A","value":13,"colname":"level3"},{"name":"mister_k","group":"A","value":13,"colname":"level3"},{"name":"mister_l","group":"D","value":25,"colname":"level3"},{"name":"mister_m","group":"D","value":16,"colname":"level3"},{"name":"mister_n","group":"D","value":28,"colname":"level3"}],"colname":"level2"}]
+,"name":"CEO"}
